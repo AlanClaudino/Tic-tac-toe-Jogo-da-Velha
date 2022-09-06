cb3e3e0736ca9a5dd7e54a02b1a37f9ccfed4ede
Alan Claudino <alanrodrigoclaudino@gmail.com>
2022-09-06 16:16:12 -0300

    implements game logic

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,19 @@
 const startGame = document.getElementById('start-game');
 const mainContent = document.getElementById('main-content');
 const newGame = document.getElementById('new-game');
+const boardSpace = document.querySelectorAll('.board-space');
+const playerX = document.getElementById('player-x');
+const playerO = document.getElementById('player-o');
+const winnerMessage = document.getElementById('winner-message');
 
 const playBtn = document.getElementById('play-btn');
 const newGameBtn = document.getElementById('new-game-btn');
+
+let playerControl = 1;
+let xRound = 1;
+let yRound = 1;
+let gameControl = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+
 
 // Functions
 
@@ -22,6 +32,75 @@ function fadeINOut(fadeIn, fadeOut) {
   }, {once: true});
 }
 
+function updatePlayer() {
+  if(playerControl === 1) {
+    playerControl = 2;
+    playerX.classList.remove('underline');
+    playerO.classList.add('underline');
+  } else {
+    playerControl = 1;
+    playerO.classList.remove('underline');
+    playerX.classList.add('underline');
+  }
+}
+
+function write(boardSpace) {
+  if (boardSpace.innerText != "") return;
+  if (playerControl === 1) {
+    boardSpace.innerText = "X"    
+  } else {
+    boardSpace.innerText = "O";
+  }
+}
+
+function updateGameControl(boardSpaceId) {
+  let line = Math.trunc(boardSpaceId / 3);
+  let column = Math.trunc(boardSpaceId % 3);
+  console.log (line, column)
+  if (playerControl === 1) {
+    gameControl[line][column] = "x";
+  } else {
+    gameControl[line][column] = "o";
+  }
+}
+
+function winnerCheck(boardSpaceId) {
+  let line = Math.trunc(boardSpaceId / 3);
+  let column = Math.trunc(boardSpaceId % 3);
+  if (playerControl === 1) {
+    if (gameControl[line][0] === gameControl[line][1] && gameControl[line][1] === gameControl[line][2]) {
+      winnerMessage.innerText = "Player X won!"
+      fadeINOut (newGame, mainContent)
+    } else if (gameControl[0][column] === gameControl[1][column] && gameControl[1][column] === gameControl[2][column]) {
+      winnerMessage.innerText = "Player X won!"
+      fadeINOut (newGame, mainContent)
+    } else if (gameControl[0][0] === "x" && gameControl[1][1] === "x" && gameControl[2][2] === "x") {
+      winnerMessage.innerText = "Player X won!"
+      fadeINOut (newGame, mainContent)
+    } else if (gameControl[0][2] === "x" && gameControl[1][1] === "x" && gameControl[2][0] === "x") {
+      winnerMessage.innerText = "Player X won!"
+      fadeINOut (newGame, mainContent)
+    } else {
+      return
+    }
+  } else if (playerControl === 2) {
+    if (gameControl[line][0] === gameControl[line][1] && gameControl[line][1] === gameControl[line][2]) {
+      winnerMessage.innerText = "Player O won!"
+      fadeINOut (newGame, mainContent)
+    } else if (gameControl[0][column] === gameControl[1][column] && gameControl[1][column] === gameControl[2][column]) {
+      winnerMessage.innerText = "Player O won!"
+      fadeINOut (newGame, mainContent)
+    } else if (gameControl[0][0] === "o" && gameControl[1][1] === "o" && gameControl[2][2] === "o") {
+      winnerMessage.innerText = "Player O won!"
+      fadeINOut (newGame, mainContent)
+    } else if (gameControl[0][2] === "o" && gameControl[1][1] === "o" && gameControl[2][0] === "o") {
+      winnerMessage.innerText = "Player O won!"
+      fadeINOut (newGame, mainContent)
+    } else {
+      return
+    }
+  }
+}
 // Events
 
 playBtn.addEventListener('click', () => {
@@ -32,6 +111,15 @@ newGame.addEventListener('click', () => {
   fadeINOut(mainContent, newGame)
 })
 
-
+boardSpace.forEach( (key) => {
+  key.addEventListener('click', () => {
+    let keyId = parseInt(key.id)
+    console.log(keyId)
+    write(key);
+    updateGameControl(keyId);
+    winnerCheck(keyId)
+    updatePlayer();
+  })
+})
 
 
